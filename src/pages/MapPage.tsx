bd968161ace6e5036f65c02952f885f3783079ed
Finnik <73883383+golkity@users.noreply.github.com>
2025-06-10 19:01:08 +0300

    Add: files for Map pages

---
+++ src/pages/MapPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Map3D from '../components/Map3D';
+import './MapPage.scss';
+
+const MapPage: React.FC = () => {
+  const apiKey = process.env.REACT_APP_MAP_API_KEY || 'API ADD';//TODO: ADD API
+
+  return (
+    <div className="map-page">
+      <Map3D apiKey={apiKey} />
+    </div>
+  );
+};
+
+export default MapPage; 
